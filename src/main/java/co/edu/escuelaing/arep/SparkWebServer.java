c5704a34416aeafc1b2c4e5ab7d356e1c42825f7
Santiago <xxsantirocha12xx@gmail.com>
2023-09-19 15:51:11 -0500

    Formulario añadido

--- src/main/java/co/edu/escuelaing/arep/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/arep/SparkWebServer.java
@@ -2,10 +2,31 @@ package co.edu.escuelaing.arep;
 
 import static spark.Spark.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class SparkWebServer {
     
     public static void main(String... args){
         port(getPort());
+
+        get("/", (req, res) -> {
+            try {
+                res.type("text/html");
+                StringBuilder content = new StringBuilder();
+                BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\co\\edu\\escuelaing\\arep\\formularios.html"));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    content.append(line);
+                }
+                br.close();
+                return content.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Error al cargar el archivo HTML";
+            }
+        });
+
         get("hello", (req,res) -> "Hello Docker!");
 
         get("sin", (req,res) -> Functionalities.sin(req.queryParams("number")));
